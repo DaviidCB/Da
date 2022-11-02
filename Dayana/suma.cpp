8f4282e9cd4dee98aa7d344b265179a0680992f9
Dayana <you@example.com>
2022-11-02 11:45:38 -0500

    version1

---
+++ Dayana/suma.cpp
@@ -0,0 +1,12 @@
+nclude <iostream>
+using namespace std;
+int main ()
+{
+		float A,B,C;
+			cout<<"Ingrese dos numeros A B: ";
+				cin>>A>>B;
+					C=A+B;
+						cout<<"El resultado es: ";
+							cout<<C;
+								return 0;
+}
